4ba8a3f5c09971a4855dcdc9c3eed556e1316761
Woo Song <ws392@drexel.edu>
2020-01-30 14:08:33 -0500

    add heading vars in typo.js and apply to profile header

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,24 +1,35 @@
-import React from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
-import { Colors, Typography, Layouts, Mixins } from "../styles/index";
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+import { Colors, Typography, Layouts, Mixins } from '../styles/index'
 
 export default UserProfile = props => {
   return (
     <View style={[styles.userProfile, Layouts.FLEX_CONTAINER_CENTER]}>
       <Image
         style={styles.userProfile__image}
-        source={{ uri: "https://picsum.photos/id/237/300/300" }}
+        source={{ uri: 'https://picsum.photos/id/237/300/300' }}
       />
-      <Text>User Profile</Text>
+      <View
+        style={[
+          styles.userProfile__text_wrapper,
+          Layouts.FLEX_CONTAINER_CENTER,
+        ]}
+      >
+        <Text style={Typography.F_H2}>User Profile</Text>
+        <Text style={Typography.F_BODY}>
+          some bio information...lorem ipsum stuff...
+        </Text>
+      </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   userProfile: {},
+  userProfile__text_wrapper: {},
   userProfile__image: {
     width: 200,
     height: 200,
-    borderRadius: 100
-  }
-});
+    borderRadius: 100,
+  },
+})
--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,14 +1,34 @@
-import { scaleFont } from "./mixins";
+import { scaleFont } from './mixins'
 
 // FONT WEIGHT
-export const F_WEIGHT_REGULAR = "400";
-export const F_WEIGHT_BOLD = "700";
+export const F_WEIGHT_REGULAR = '400'
+export const F_WEIGHT_BOLD = '700'
 
 // FONT SIZE
-export const F_SIZE_24 = scaleFont(24);
+export const F_SIZE_24 = scaleFont(24)
+export const F_SIZE_20 = scaleFont(20)
+export const F_SIZE_18 = scaleFont(18)
+export const F_SIZE_16 = scaleFont(16)
+export const F_SIZE_14 = scaleFont(14)
 
 // HEADING STYLES
-export const F_H2 = {
+
+export const F_H1 = {
   fontSize: F_SIZE_24,
-  fontWeight: F_WEIGHT_BOLD
-};
+  fontWeight: F_WEIGHT_BOLD,
+}
+
+export const F_H2 = {
+  fontSize: F_SIZE_20,
+  fontWeight: F_WEIGHT_REGULAR,
+}
+
+export const F_H3 = {
+  fontSize: F_SIZE_20,
+  fontWeight: F_WEIGHT_BOLD,
+}
+
+export const F_BODY = {
+  fontSize: F_SIZE_18,
+  fontWeight: F_WEIGHT_REGULAR,
+}
