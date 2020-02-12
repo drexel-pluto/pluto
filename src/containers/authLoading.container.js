32affa99f66eeb3a48d0cc189e9610e77be6466c
Clay Tercek <code@claytercek.com>
2020-02-11 19:39:21 -0500

    fix expired token logic

--- src/containers/authLoading.container.js
+++ src/containers/authLoading.container.js
@@ -11,7 +11,7 @@ class AuthLoadingContainer extends React.Component {
   componentDidMount() {
     this.props.init().then(() => {
       this.props.navigation.navigate(
-        this.props.token !== null ? 'App' : 'Login'
+        this.props.token !== null && this.props.loggedIn ? 'App' : 'Login'
       )
     })
   }
