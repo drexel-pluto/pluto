9b70ea86635b3d0e233d2b130453a3b3ed2becd4
Woo Song <ws392@drexel.edu>
2020-02-14 16:26:51 -0500

    simplify iconbutton compo

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -5,94 +5,20 @@ import { Colors, Typography, Layouts, Mixins } from '../styles/index'
 export default IconButton = props => {
   const { type, _onPress } = props
 
-  switch (type) {
-    case 'search':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>Search</Text>
-        </TouchableOpacity>
-      )
-
-    case 'noti':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>Noti</Text>
-        </TouchableOpacity>
-      )
-
-    case 'filter':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>Filter</Text>
-        </TouchableOpacity>
-      )
-
-    case 'profile':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>Profile</Text>
-        </TouchableOpacity>
-      )
-
-    case 'back':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress(null)
-          }}
-        >
-          <Text>Back</Text>
-        </TouchableOpacity>
-      )
-
-    case 'setting':
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>setting</Text>
-        </TouchableOpacity>
-      )
-
-    default:
-      return (
-        <TouchableOpacity
-          style={styles.iconButton}
-          onPress={() => {
-            _onPress()
-          }}
-        >
-          <Text>icon</Text>
-        </TouchableOpacity>
-      )
-  }
+  return (
+    <TouchableOpacity
+      style={styles.iconButton}
+      onPress={() => {
+        type == 'back' ? _onPress(null) : _onPress()
+      }}
+    >
+      <Text>{type}</Text>
+    </TouchableOpacity>
+  )
 }
 
 IconButton.defaultProps = {
+  type: 'icon',
   _onPress: () => {},
 }
 
