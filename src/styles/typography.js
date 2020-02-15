e8cd640ebcc8c3e22af04d83f4c133db02b38f79
Woo Song <ws392@drexel.edu>
2020-02-15 13:55:13 -0500

    add typo system

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -11,24 +11,46 @@ export const F_SIZE_18 = scaleFont(18)
 export const F_SIZE_16 = scaleFont(16)
 export const F_SIZE_14 = scaleFont(14)
 
-// HEADING STYLES
+// LINE HEIGHT
+export const F_LH_40 = scaleFont(40)
+export const F_LH_32 = scaleFont(32)
+export const F_LH_28 = scaleFont(28)
+export const F_LH_24 = scaleFont(24)
+export const F_LH_18 = scaleFont(18)
 
+// HEADING STYLES
 export const F_H1 = {
   fontSize: F_SIZE_24,
   fontWeight: F_WEIGHT_BOLD,
+  lineHeight: F_LH_40,
 }
 
 export const F_H2 = {
   fontSize: F_SIZE_20,
   fontWeight: F_WEIGHT_REGULAR,
+  lineHeight: F_LH_32,
 }
 
 export const F_H3 = {
-  fontSize: F_SIZE_20,
+  fontSize: F_SIZE_18,
   fontWeight: F_WEIGHT_BOLD,
+  lineHeight: F_LH_32,
 }
 
 export const F_BODY = {
   fontSize: F_SIZE_18,
   fontWeight: F_WEIGHT_REGULAR,
+  lineHeight: F_LH_28,
+}
+
+export const F_SUBTITLE = {
+  fontSize: F_SIZE_16,
+  fontWeight: F_WEIGHT_REGULAR,
+  lineHeight: F_LH_24,
+}
+
+export const F_CAPTION = {
+  fontSize: F_SIZE_14,
+  fontWeight: F_WEIGHT_REGULAR,
+  lineHeight: F_LH_18,
 }
