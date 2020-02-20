d1dfb5ced1c7a902913b4c97276c5693d072993a
Snibeti <snibeti>
2020-02-20 14:41:40 -0500

    add icon compo

--- src/components/GroupPanel.js
+++ src/components/GroupPanel.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Colors, Typography, Layouts, Mixins } from '../styles/index'
+import Icon from './Icon'
 
 export default GroupPanel = props => {
   return (
     <View style={styles.groupPanel}>
       <Text style={[styles.title, Typography.F_H2]}>Group Titleeeeee</Text>
       <Text style={[styles.detail]}>Updates from group</Text>
+      <Icon name={'heart'} />
     </View>
   )
 }
---
+++ src/components/Icon.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Colors, Typography, Layouts, Mixins } from '../styles/index'
+
+export default Icon = props => {
+  return (
+    <View style={styles.icon}>
+      {
+        //conditionals for diff icon names
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  icon: {},
+})
