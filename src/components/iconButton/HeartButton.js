8a26c7e2362753c5af62a5e4ca67830137578064
Woo Song <ws392@drexel.edu>
2020-03-09 23:08:20 -0400

    #142 heartbutton animation

--- src/components/iconButton/HeartButton.js
+++ src/components/iconButton/HeartButton.js
@@ -1,16 +1,85 @@
 import React from 'react'
-import { View, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native'
+import {
+  View,
+  TouchableWithoutFeedback,
+  Animated,
+  StyleSheet,
+} from 'react-native'
 import { Colors, Typography, Layouts, Mixins, Styles } from '../../styles/index'
 import IconHeart from '../../assets/images/iconHeart.svg'
+import IconHeartFilled from '../../assets/images/iconHeartFilled.svg'
 
-export default HeartButton = props => {
-  return (
-    <TouchableWithoutFeedback style={styles.like}>
-      <IconHeart />
-    </TouchableWithoutFeedback>
-  )
+class HeartButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.animatedScaleValue = new Animated.Value(0)
+    this.animatedTranslateValue = new Animated.Value(0)
+  }
+
+  heartAnimate() {
+    Animated.spring(this.animatedScaleValue, {
+      toValue: 1,
+    }).start()
+
+    Animated.spring(this.animatedTranslateValue, {
+      toValue: 5,
+      delay: 300,
+      friction: 3,
+      tension: 40,
+    }).start()
+  }
+
+  translate() {
+    Animated.spring(this.animatedTranslateValue, {
+      toValue: 5,
+      // friction: 3,
+      // tension: 40,
+    }).start()
+  }
+
+  render() {
+    const animatedStyleScale = {
+      transform: [
+        {
+          scale: this.animatedScaleValue,
+        },
+      ],
+    }
+
+    const animatedStyleTranslate = {
+      transform: [
+        {
+          scale: this.animatedScaleValue,
+        },
+        { translateX: this.animatedTranslateValue },
+      ],
+    }
+
+    return (
+      <TouchableWithoutFeedback onPress={() => this.heartAnimate()}>
+        <View style={styles.heartButton}>
+          <IconHeart />
+          <Animated.View style={[styles.filled, animatedStyleTranslate]}>
+            <Animated.View style={[styles.filled, animatedStyleScale]}>
+              <IconHeartFilled />
+            </Animated.View>
+          </Animated.View>
+        </View>
+      </TouchableWithoutFeedback>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  like: {},
+  heartButton: {
+    position: 'relative',
+  },
+  filled: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: -10,
+  },
 })
+
+export default HeartButton
