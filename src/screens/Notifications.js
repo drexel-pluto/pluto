65d44bf6db70984466d76864e10590f24a6482ef
nicaglin <nicaglin@gmail.com>
2020-03-10 19:05:46 -0400

    notifications styled

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
 import { Colors, Typography, Layouts, Mixins, Styles } from '../styles/index'
 import { LinearGradient } from 'expo-linear-gradient'
 import IconButton from '../components/iconButton/IconButton'
-
+import AuthorHeader from '../components/AuthorHeader'
 
 
 class Notifications extends React.Component {
@@ -20,11 +20,28 @@ class Notifications extends React.Component {
             end={{ x: 1, y: 0 }}
             locations={[0, 0.5]}
             >
-            <View style={styles.heading}>
-                <Text style={Typography.F_H1}>notifications</Text>
-                <IconButton type="x"/>
+            <View style={styles.screenContainer}>
+                <View style={styles.heading}>
+                    <Text style={Typography.F_H1}>notifications</Text>
+                    <IconButton type="x"/>
+                </View>
+                <View style={{paddingBottom: Layouts.PAD_VERT}}>
+                    <View style={styles.notificationContainer}>
+                        <Image
+                            style={[styles.image]}
+                            source={{
+                                uri: 'https://picsum.photos/id/237/300/300',
+                            }}
+                            />
+                        <View>
+                            <Text style={[Typography.F_BODY, {maxWidth: Mixins.scaleSize(230)}]}>Friendly Person liked your post</Text>
+                            <Text style={Typography.F_CAPTION}>2 hrs ago</Text>
+                        </View>
+                    </View>
+                </View>
+               
+
             </View>
-          
           </LinearGradient>
         </View>
       )
@@ -32,10 +49,30 @@ class Notifications extends React.Component {
   }
 
   const styles = StyleSheet.create({
+    screenContainer: {
+        paddingHorizontal: Layouts.PAD_HORZ,
+    },
     heading: {
         flexDirection: 'row',
-        paddingTop: 80
+        paddingTop: Mixins.scaleSize(60),
+        paddingHorizontal: Layouts.PAD_HORZ,
+        justifyContent: 'space-between',
     },
+    notificationContainer: {
+        backgroundColor: Colors.CREAM,
+        paddingHorizontal: Layouts.PAD_HORZ,
+        paddingVertical: Layouts.PAD_VERT,
+        borderRadius: Mixins.scaleSize(60),
+        borderWidth: Mixins.scaleSize(1),
+        borderColor: Colors.MELON.dark,
+        flexDirection: 'row',
+    }, 
+    image: {
+        width: Mixins.scaleSize(45),
+        height: Mixins.scaleSize(45),
+        borderRadius: Mixins.scaleSize(45) / 2,
+        marginRight: Mixins.scaleSize(15),
+      },
   })
   
   
