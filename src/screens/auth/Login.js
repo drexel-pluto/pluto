cd9e98c8b8e33a5e49e67e8f6fa2f9fef79e1af4
nicaglin <nicaglin@gmail.com>
2020-03-10 21:00:20 -0400

    login style started

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
-import { Button, TextInput, View, Text } from 'react-native'
+import React from 'react'
+import { View, StyleSheet, Text, TextInput, Image } from 'react-native'
+import { Colors, Typography, Layouts, Mixins, Styles } from '../../styles/index'
+import { LinearGradient } from 'expo-linear-gradient'
+import Button from '../../components/Button'
 
-export default class LoginScreen extends Component {
+export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = { username: '', password: '' }
@@ -17,53 +20,82 @@ export default class LoginScreen extends Component {
   }
 
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', marginTop: 50 }}>
-        {this.props.error.length > 0 && (
-          <View style={{ backgroundColor: 'red' }}>
-            <Text>{this.props.error}</Text>
+    return ( 
+    
+    <LinearGradient
+        colors={Colors.gradient.light(Colors.VIOLET)} style={{flex:1}}>
+      <View style={[styles.screenContainer]}>
+        <Text style={[Typography.F_H1,{color: Colors.CREAM, paddingBottom: Layouts.PAD_VERT}] }>login</Text>
+        <View style={{paddingBottom: Layouts.PAD_VERT}}>
+          <View style={{paddingBottom: Layouts.PAD_VERT}}>        
+            <TextInput
+              style={[styles.input, Typography.F_BODY]}
+              placeholder="username/email/phone"
+              placeholderTextColor={Colors.CREAM}
+              onChangeText={username => this.setState({ username })}
+              autoCompleteType="username"
+              autoCapitalize="none"
+              autoCorrect={false}
+              textContentType="username"
+            />
           </View>
-        )}
-        <TextInput
-          style={{
-            height: 40,
-            width: '60%',
-            backgroundColor: '#EEE',
-            marginBottom: 6,
-          }}
-          placeholder="Insert username"
-          onChangeText={username => this.setState({ username })}
-          autoCompleteType="username"
-          autoCapitalize="none"
-          autoCorrect={false}
-          textContentType="username"
-        />
-        <TextInput
-          style={{
-            height: 40,
-            width: '60%',
-            backgroundColor: '#EEE',
-            marginBottom: 6,
-          }}
-          placeholder="Insert password"
-          onChangeText={password => this.setState({ password })}
-          autoCompleteType="password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          textContentType="password"
-          secureTextEntry={true}
-        />
+          <View style={{paddingBottom: Layouts.PAD_VERT}}>        
+            <TextInput
+              style={[styles.input, Typography.F_BODY]}
+              placeholder="password"
+              placeholderTextColor={Colors.CREAM}
+              onChangeText={password => this.setState({ password })}
+              autoCompleteType="password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              textContentType="password"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <View style={{paddingBottom: Layouts.PAD_VERT}}>
+          {this.props.error.length >-0 && (
+            <View style={styles.error}>
+              <Text style={{color: Colors.MELON.dark}}>{this.props.error}</Text>
+            </View>
+          )}
+        </View>
+        <View style={{paddingBottom: Layouts.PAD_VERT}}>
         <Button
-          onPress={() =>
+          _onPress={() =>
             this.props.login(this.state.username, this.state.password)
           }
-          title="Click here to login"
+          text="login"
         />
+        </View>
         <Button
-          onPress={() => this.props.setIsCreate(true)}
-          title="create account"
+          _onPress={() => this.props.setIsCreate(true)}
+          text="create account"
+          type="outline"
         />
       </View>
+      </LinearGradient>
     )
   }
 }
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1, 
+    alignItems: 'center', 
+    paddingVertical: 90,
+    // backgroundColor: Colors.VIOLET.dark,
+  },
+  input: {
+    height: 40,
+    paddingVertical: Layouts.PAD_VERT,
+    paddingHorizontal: Layouts.PAD_HORZ,
+    width: Mixins.scaleSize(220),
+    borderRadius: Mixins.scaleSize(60),
+    borderWidth: Mixins.scaleSize(1),
+    borderColor: Colors.CREAM,   
+  },
+  error: {
+    paddingVertical: Layouts.PAD_VERT,
+    color: Colors.MELON.dark,
+  },
+})
